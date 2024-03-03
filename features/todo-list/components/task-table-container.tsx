04d16ad442823f907b7f4c9beb2c45b2f685e491
Kohei Omachi <kohei.omachi@gmail.com>
2024-03-03 19:10:21 +0900

    refactor: getServerAuthSession

--- features/todo-list/components/task-table-container.tsx
+++ features/todo-list/components/task-table-container.tsx
@@ -1,12 +1,10 @@
 import { deleteTaskAction } from '@/features/todo-list/actions/delete-task-action'
 import { updateTaskStatusAction } from '@/features/todo-list/actions/update-task-action'
 import { TaskTable } from '@/features/todo-list/components/task-table'
-import { getServerAuthSession } from '@/libs/next-auth/auth'
-
+import { checkAuthenticatedUser } from '@/libs/next-auth/auth'
 
 export const TaskTableContainer = async () => {
-  const session = await getServerAuthSession()
-  const userId = session?.user?.id
+  const userId = await checkAuthenticatedUser()
   if (userId === undefined) return <></>
   // TODO: タスク一覧を取得する
 
